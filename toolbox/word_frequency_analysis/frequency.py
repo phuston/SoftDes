468aab78e2a182e984855f9cd62bd0387d509fe5
Patrick Huston <patrickhuston1@gmail.com>
2015-03-21 11:01:50 -0400

    Completes word frequency analysis toolbox

--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -2,6 +2,7 @@
 	Project Gutenberg """
 
 import string
+from collections import Counter
 
 def get_word_list(file_name):
 	""" Reads the specified project Gutenberg book.  Header comments,
@@ -9,7 +10,21 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	word_list = []
+	f = open(file_name,'r')
+	lines = f.readlines()
+	curr_line = 0
+	while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
+		curr_line += 1
+	lines = lines[curr_line+1:]
+
+	for line in lines:
+		line = line.lower()
+		for char in (string.punctuation):
+			line.replace(char, "")
+		word_list += line.split()
+
+	return word_list
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -21,4 +36,10 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+	uniqueWords = set(word_list)
+	counts = sorted([(word_list.count(word), word) for word in uniqueWords])
+	return counts[len(counts)-n:][::-1]
+
+
+if __name__ == "__main__":
+	print get_top_n_words(get_word_list('pg32325.txt'),20)
